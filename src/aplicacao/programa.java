76bfbde49ac4a9cba1f25fb06c553345d298c70f
Lucas Borges <lucas.a.b.p1@gmail.com>
2019-03-24 21:42:51 -0300

    Mensagem de boa noite

--- src/aplicacao/programa.java
+++ src/aplicacao/programa.java
@@ -3,10 +3,9 @@ package aplicacao;
 public class programa {
 
 	public static void main(String[] args) {
-	
-		System.out.println("Bom dia !");
-		System.out.println("Boa tarde !");
-
+		
+		System.out.println("Bom dia!");
+		System.out.println("Boa tarde!");
+		System.out.println("Boa noite!");
 	}
-
 }
